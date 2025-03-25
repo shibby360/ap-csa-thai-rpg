5dff352c6b85434d8c6edeb88337537d6d610bf4
Shivank Chhaya <shivank.chhaya@gmail.com>
2025-03-25 21:05:56 +0000

    added simple level playing

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,7 +1,7 @@
 public class Enemy extends Entity {
     String type;
     public Enemy(int h, int d, String t) {
-        super(h,d);
+        super(h*(t=="boss"?3:1),d*(t=="boss"?2:1));
         type = t;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        String name = Tools.input("whats your name? ");
-        Weapon sword = new Weapon(0, "ffer");
-        Player p = new Player(100, 5, name);
+        // String name = Tools.input("whats your name? ");
+        String name = "streamline";
+        Weapon sword = new Weapon(0, "sword");
         while(true) {
             Tools.clear();
             String c = Tools.input("[1]level\n[2]check stats\n[3]exit\n");
             if(c.equals("1")) {
                 String lc = Tools.input("pick a level 1-2: \n");
-                while(p.health > 0) {
-                    if() {
-
+                Enemy[] currlevel = Levels.levels[Integer.parseInt(lc)-1];
+                Player p = new Player(100, 5, name);
+                for(Enemy x : currlevel) {
+                    while(x.health > 0 && p.health > 0) {
+                        System.out.println(x.type.toUpperCase()+"\nhealth: " + p.health + "\nenemy health: " + x.health);
+                        p.attack(x);
+                        x.attack(p);
+                        Tools.waitEnter();
+                        Tools.clear();
                     }
+                    if(p.health <= 0) {
+                        System.out.println("you got deaded :(");
+                        break;
+                    }
+                }
+                if(p.health > 0) {
+                    System.out.println("you killed them all yay");
                 }
                 Tools.waitEnter();
             } else if(c.equals("2")) {
